be175fe527c623f9e518b77979f73ca0f3408879
Abu Chodisetti <sravanth@gmail.com>
2011-09-08 22:44:47 -0700

    First rails checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icoddle::Application.config.secret_token = 'a2c172fb8989b201be76b36570c6b09020b33317cabdba9787ec705e289de980442bcdc57bef7d4e9342f92a80917f5b91e7fd72e099ba089057b413b0587af3'
