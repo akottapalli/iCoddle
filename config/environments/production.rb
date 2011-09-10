461e9631b78b0e6c750158623cfb297dbe0f3f6a
Abu Chodisetti <sravanth@gmail.com>
2011-09-10 14:37:18 -0700

    Finally got layout.css.scss file precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Icoddle::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile += %w( layout.scss )
+  #config.assets.precompile += ['layout.scss']
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -44,7 +44,7 @@ Icoddle::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( layout.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
