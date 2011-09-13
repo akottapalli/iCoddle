c63b4405d6a193a4c848e46e2723fee48872f61e
Abu Chodisetti <sravanth@gmail.com>
2011-09-13 00:39:10 -0700

    Adding register/login functionality

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'haml-rails'
 
 gem 'sass'
 
+gem 'authlogic'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Icoddle::Application.routes.draw do
+  resources :users, :user_sessions
+  match 'login' => 'user_sessions#new', :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
+
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +54,7 @@ Icoddle::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'users#index', :as => :homepage
 
   # See how all your routes lay out with "rake routes"
 
