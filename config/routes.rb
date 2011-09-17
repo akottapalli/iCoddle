a7f8f025f8c0f6b6de6601a551ae66174be229be
Abu Chodisetti <sravanth@gmail.com>
2011-09-17 14:49:16 -0700

    Since the new root is leads page, made the logout page redirect to leads on logout Need :homepage setup in routes since logout redirects to :homepage

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Icoddle::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'users#index', :as => :homepage
-  root :to => 'leads#new'
+  root :to => 'leads#new', :as => :homepage
 
   # See how all your routes lay out with "rake routes"
 
