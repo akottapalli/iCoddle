65163efb5a827ef378a5a1c61d31fa000357dd6f
Abu Chodisetti <sravanth@gmail.com>
2011-09-18 17:22:53 -0700

    User sessions only needs new/create/destroy - removed other actions

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,25 +1,4 @@
 class UserSessionsController < ApplicationController
-  # GET /user_sessions
-  # GET /user_sessions.json
-  def index
-    @user_sessions = UserSession.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @user_sessions }
-    end
-  end
-
-  # GET /user_sessions/1
-  # GET /user_sessions/1.json
-  def show
-    @user_session = UserSession.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user_session }
-    end
-  end
 
   # GET /user_sessions/new
   # GET /user_sessions/new.json
@@ -30,11 +9,6 @@ class UserSessionsController < ApplicationController
       format.html # new.html.erb
       format.json { render json: @user_session }
     end
-  end
-
-  # GET /user_sessions/1/edit
-  def edit
-    @user_session = UserSession.find(params[:id])
   end
 
   # POST /user_sessions
@@ -48,22 +22,6 @@ class UserSessionsController < ApplicationController
         format.json { render json: @user_session, status: :created, location: @user_session }
       else
         format.html { render action: "new" }
-        format.json { render json: @user_session.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /user_sessions/1
-  # PUT /user_sessions/1.json
-  def update
-    @user_session = UserSession.find(params[:id])
-
-    respond_to do |format|
-      if @user_session.update_attributes(params[:user_session])
-        format.html { redirect_to @user_session, notice: 'User session was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
         format.json { render json: @user_session.errors, status: :unprocessable_entity }
       end
     end
