daf33ca19f7cce4f952a869d8d669f1490ca65d0
Abu Chodisetti <sravanth@gmail.com>
2011-09-21 00:55:45 -0700

    Adding new css files for precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Icoddle::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( layout.css )
+  config.assets.precompile += %w( layout.css pets.css users.css leads.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
