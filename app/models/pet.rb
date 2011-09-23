d4e75885e5390d69f7ae4a8432ffbe58a0031ac8
Abu Chodisetti <sravanth@gmail.com>
2011-09-22 22:19:11 -0700

    Integrating with S3

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,12 @@
 class Pet < ActiveRecord::Base
   belongs_to :user
   has_attached_file :photo,
-    :styles => { :medium => "300x300>", :thumb => "100x100>" }
-    #:storage => :s3,
-    #:s3_credentials => "#{Rails.root}/config/s3.yml",
-    #:path => "/:style/:id/:filename"
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :storage => :s3,
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    },
+    :bucket => ENV['S3_BUCKET'], 
+    :path => "/:style/:id/:filename"
 end
