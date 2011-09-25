6caa35d22d4f51eaaaacfd61be701e99eec11b7d
Abu Chodisetti <sravanth@gmail.com>
2011-09-25 16:21:14 -0700

    Fixes to make self-referencing work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   has_many :pets, :dependent => :destroy
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 end
